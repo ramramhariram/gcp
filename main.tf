d7a8467e45e334f38f18392c7ee929e0bae98f74
Hariram Sankaran <hariram@Haris-MacBook.home>
2020-09-02 17:04:45 -0700

    deploying gcp

--- main.tf
+++ main.tf
@@ -13,3 +13,27 @@ module "aws_transit_1" {
   egress_enabled   = var.egress_enabled
   fw_instance_size = var.fw_instance_size
 }
+
+# GCP Transit Module
+module "gcp_transit_1" {
+  source           = "github.com/terraform-aviatrix-modules/terraform-aviatrix-gcp-transit"
+  #version         = "1.0.1"
+  ha_gw            = true
+  ha_region        = var.gcp_second_region
+  sub1_cidr        = var.gcp_sub1_cidr
+  sub2_cidr        = var.gcp_sub2_cidr
+  primary_region   = var.gcp_primary_region
+  account          = var.gcp_account_name
+}
+
+# GCP Spoke Module
+module "gcp_spoke_1" {
+  source           = " github.com/terraform-aviatrix-modules/terraform-aviatrix-gcp-spoke"
+  #version         = "1.0.1"
+  gcp_account_name = var.gcp_account_name
+  gcp_spoke_region = var.gcp_spoke_region
+  gcp_spoke_sub1_cidr = var.gcp_spoke_sub1_cidr
+  transit_gateway = module.gcp_transit_1.transit_gateway
+}
+
+
